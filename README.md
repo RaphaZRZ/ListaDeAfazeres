1c11b77999d4a54acdc5764702bcb4df42713507
RaphaZ <115039334+RaphaZRZ@users.noreply.github.com>
2024-11-14 21:16:02 -0300

    conectado ao banco de dados local

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>Aplicação SpringBoot</h1>
+<h2>Importante</h2>
+<p> Para conectar ao seu banco de dados local é preciso alterar a linha `spring.datasource.password=abcd1234`
+    localizada em `src/main/resources/application.properties` para a seguinte linha: `spring.datasource.password=root`
+</p>
--- src/main/java/com/raphazrz/todosimple/listaDeAfazeres.java
+++ src/main/java/com/raphazrz/todosimple/listaDeAfazeres.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodosimpleApplication {
+public class listaDeAfazeres {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodosimpleApplication.class, args);
+		SpringApplication.run(listaDeAfazeres.class, args);
 	}
 
 }
